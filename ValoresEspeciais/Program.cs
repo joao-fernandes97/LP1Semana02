c432e8642aea258516c462e384d57e6adbc9c1ba
joao-fernandes97 <joaofrfernandes.work@gmail.com>
2024-03-01 17:09:29 +0000

    Signed whole numbers maxs and mins

--- ValoresEspeciais/Program.cs
+++ ValoresEspeciais/Program.cs
@@ -6,7 +6,18 @@ namespace ValoresEspeciais
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Signed whole number types
+            Console.WriteLine(sbyte.MaxValue);
+            Console.WriteLine(sbyte.MinValue);
+
+            Console.WriteLine(short.MaxValue);
+            Console.WriteLine(short.MinValue);
+
+            Console.WriteLine(int.MaxValue);
+            Console.WriteLine(int.MinValue);
+
+            Console.WriteLine(long.MaxValue);
+            Console.WriteLine(long.MinValue);
         }
     }
 }
