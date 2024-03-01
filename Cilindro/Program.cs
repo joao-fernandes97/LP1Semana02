8cbde4ace15b947fabba6b2aac36cd39a6d4af89
joao-fernandes97 <joaofrfernandes.work@gmail.com>
2024-03-01 16:57:35 +0000

    Convert input and calculate cylinder volume

--- Cilindro/Program.cs
+++ Cilindro/Program.cs
@@ -7,10 +7,15 @@ namespace Cilindro
         static void Main(string[] args)
         {
             Console.Write("Altura do cilindro:");
-            string h = Console.ReadLine();
+            string height = Console.ReadLine();
+            float a = float.Parse(height);
 
             Console.Write("Raio do cilindro:");
-            string r = Console.ReadLine();
+            string radius = Console.ReadLine();
+            float r = float.Parse(radius);
+            
+            float vol= MathF.PI*MathF.Pow(r,2)*a;
+            Console.WriteLine(vol);
         }
     }
 }
