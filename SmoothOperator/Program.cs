dc35ced4ee740927b62916c692950f9ac82be5e9
joao-fernandes97 <joaofrfernandes.work@gmail.com>
2024-03-01 16:27:09 +0000

    Prompts user and reads response

---
+++ SmoothOperator/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SmoothOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Insere numero nao-negativo:" );
+            string str = Console.ReadLine();
+            
+        }
+    }
+}
