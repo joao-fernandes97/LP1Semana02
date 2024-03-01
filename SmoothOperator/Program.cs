3c9474de6cb1f51bfe64b4dbeba54117de99900d
joao-fernandes97 <joaofrfernandes.work@gmail.com>
2024-03-01 16:42:32 +0000

    Displays check for if the value is larger than 10

--- SmoothOperator/Program.cs
+++ SmoothOperator/Program.cs
@@ -15,6 +15,7 @@ namespace SmoothOperator
             Console.WriteLine(n/2);
             Console.WriteLine(n<<3);
             Console.WriteLine(n^5);
+            Console.WriteLine(n>10);
         }
     }
 }
