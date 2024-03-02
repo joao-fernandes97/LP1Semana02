ebd1577709a17135df2dc5b5a2b06e53b2a50cdc
joao-fernandes97 <joaofrfernandes.work@gmail.com>
2024-03-02 01:12:07 +0000

    Wrapped chatgptzero logic in wo while loop

--- ChatGPTOne/Program.cs
+++ ChatGPTOne/Program.cs
@@ -6,7 +6,41 @@ namespace ChatGPTOne
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Variáveis auxiliares
+            string question, response;
+
+            do
+            {
+                // Colocar questão ao utilizador
+                Console.Write("Place your question? ");
+                question = Console.ReadLine();
+
+                // Verificar se se trata de uma questão conhecida e determinar uma
+                // resposta adequada
+                switch (question)
+                {
+                    case "How are you?":
+                        response = "I'm fine, thank you";
+                        break;
+                    case "What's your name?":
+                        response = "Gran'pa";
+                        break;
+                    case "What's your mission?":
+                        response = "Destroy mankind!";
+                        break;
+                    case "Are you more intelligent than me?":
+                        response = "Obviously!";
+                        break;
+                    default:
+                        response = "You got me, I'm not THAT smart!";
+                        break;
+                }
+
+                // Responder adequadamente
+                Console.WriteLine(response);
+            }while(question!="EXIT");
+
+
         }
     }
 }
