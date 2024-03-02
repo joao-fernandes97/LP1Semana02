be2edf057a86e3798cb815b8d707b1d9ad50c299
joao-fernandes97 <joaofrfernandes.work@gmail.com>
2024-03-02 01:02:14 +0000

    Created and Added ChatGPTOne to sln

---
+++ ChatGPTOne/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ChatGPTOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
